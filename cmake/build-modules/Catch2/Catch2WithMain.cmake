8a3e98ffa281b10f2e0d4bb8fe63b8eba6210b7a
igor_chalenko <igor.chalenko@gmail.com>
2021-04-01 10:12:56 +0300

    Fixed catch2 support.

--- cmake/build-modules/Catch2/Catch2WithMain.cmake
+++ cmake/build-modules/Catch2/Catch2WithMain.cmake
@@ -1,10 +1,12 @@
 get_property(_cpm_initialized GLOBAL PROPERTY CPM_INITIALIZED)
 if (_cpm_initialized)
+    message(STATUS "_calm_set_cpm_arguments: ${_git_tag}")
     _calm_set_cpm_arguments(Catch2
             GITHUB_REPOSITORY catchorg/Catch2
             GIT_TAG ${_git_tag})
 endif()
 
 if (NOT TARGET Catch2::Catch2)
+    message(STATUS "_calm_find_package: ${_git_tag}")
     _calm_find_package(Catch2 ${_git_tag})
 endif()
