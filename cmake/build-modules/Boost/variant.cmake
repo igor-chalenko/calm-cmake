64b61a4f9773bf1b4dbcb1d8f0f19c25d5cf2d47
igor_chalenko <igor.chalenko@gmail.com>
2021-09-03 19:06:52 +0300

    Use new CMake files in Boost instead of hand-written ones.

--- cmake/build-modules/Boost/variant.cmake
+++ cmake/build-modules/Boost/variant.cmake
@@ -2,5 +2,5 @@ if (NOT TARGET boost_variant)
     include(${_current_dir}/build-modules/Boost/internal.cmake)
     _calm_init_library(variant headers core static_assert bind mpl move detail
             functional assert type_traits type_index preprocessor config
-            throw_exception math utility)
+            throw_exception utility)
 endif()
