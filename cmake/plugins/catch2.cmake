366563287178d09a75aba7aa120adf0b79b7d70a
crusoe <sho6Feriv>
2021-10-04 01:57:15 +0300

    Catch2 reporter added.

--- cmake/plugins/catch2.cmake
+++ cmake/plugins/catch2.cmake
@@ -100,7 +100,7 @@ function(_calm_catch2_tests _for_target _sources)
     endif()
     foreach (_file IN LISTS TESTS)
         _calm_test_name_for_file(${_file} ${_target_prefix} _target)
-        catch_discover_tests(${_target})
+        catch_discover_tests(${_target} REPORTER sonarqube)
     endforeach()
 endfunction()
 
